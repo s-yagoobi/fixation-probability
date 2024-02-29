6186c821b0947f5734adbf16caddf31c25d129f3
yagoobi@evolbio.mpg.de <yagoobi@evolbio.mpg.de>
2024-02-29 10:36:05 +0100

    add plot

--- .vscode/data/plot.py
+++ .vscode/data/plot.py
@@ -1,5 +1,5 @@
 
-import numy as np
+import numpy as np
 
 numerical_fix_prob_06=np.loadtxt('star-N_5-M_4-mig-1e-06.txt')
 numerical_fix_prob_l_001=np.loadtxt('star-N_5-M_4-mig-0.01.txt')
@@ -8,6 +8,7 @@ numerical_fix_prob_l_05=np.loadtxt('star-N_5-M_4-mig-0.5.txt')
 numerical_fix_prob_l_1=np.loadtxt('star-N_5-M_4-mig-1.txt')
 
 import matplotlib.pyplot as plt
+from matplotlib.ticker import MultipleLocator
 
 fig, ax = plt.subplots(figsize=(10,7))
 ax.xaxis.set_major_locator(MultipleLocator(0.5))
@@ -24,13 +25,13 @@ plt.fill_between(np.linspace(0,1,20),y1=0,y2=ylim[1], color='whitesmoke')
 plt.fill_between(np.linspace(1.,2,20),y1=ylim[0],y2=0, color='whitesmoke')
 ax.axhline(y=0, color='k')
 
-ax.plot(Fitness1,fix_prob_star_patch(Fitness1)-fix_prob_well_mixed(Fitness1,local_size*patch_number), color='g',label='low-migration')
+#ax.plot(Fitness1,fix_prob_star_patch(Fitness1)-fix_prob_well_mixed(Fitness1,local_size*patch_number), color='g',label='low-migration')
 ax.plot(Fitness, numerical_fix_prob_06[:,1]-fix_prob_well_mixed(Fitness,local_size*patch_number) ,'P',color='tab:orange',markerfacecolor='none', label='$\lambda= 10^{-6}$')
 ax.plot(Fitness, numerical_fix_prob_l_001[:,1]-fix_prob_well_mixed(Fitness,local_size*patch_number) ,'s',color='b',markerfacecolor='none', label='$\lambda= 0.01$')
 ax.plot(Fitness,numerical_fix_prob_l_01[:,1]-fix_prob_well_mixed(Fitness,local_size*patch_number), '*',color='m',markerfacecolor='none', label='$\lambda=0.1$')
 ax.plot(Fitness, numerical_fix_prob_l_05[:,1]-fix_prob_well_mixed(Fitness,local_size*patch_number) ,'v',color='c',markerfacecolor='none', label='$\lambda= 0.5$')
 ax.plot(Fitness, numerical_fix_prob_l_1[:,1]-fix_prob_well_mixed(Fitness,local_size*patch_number) ,'o',color='darkred',markerfacecolor='none', label='$\lambda= 1$')
-ax.plot(Fitness1, phi(Fitness1,local_size,local_size*(patch_number-1))-fix_prob_well_mixed(Fitness1,local_size*patch_number), color='r', label='$\lambda= 1$')
+#ax.plot(Fitness1, phi(Fitness1,local_size,local_size*(patch_number-1))-fix_prob_well_mixed(Fitness1,local_size*patch_number), color='r', label='$\lambda= 1$')
 ax.tick_params(labelsize=12, direction='out',top=True, right=True)
 ax.set_xlabel(r'fitness ($\bf{r}$) ',fontsize=14)
 ax.set_ylabel(r"Difference in fixation probabilities, $\phi_{\bigstar}- \phi_{\rm wm} $",fontsize=14)
